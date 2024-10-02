21db43822afdf68b688d0287b936b7d2587950d1
Luis Enrique Montelongo R <itt871470@gmail.com>
2024-10-02 13:04:02 -0600

    commit inicial

---
+++ publicaciones/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class Publicacion(models.Model):
+    texto = models.TextField()
+    
+    
+    def __str__(self):
+      return self.texto[:50]
---
+++ publicaciones/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import ListView
+from .models import Publicacion
+# Create your views here.
+class VistaPaginaInicio(ListView):
+    model = Publicacion
+    template_name = 'inicio.html'
